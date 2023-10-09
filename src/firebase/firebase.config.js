55fd753e4f5f99347f84c6d2f372be0e55c0baaa
Tasin007 <mdtasinalam.mta@gmail.com>
2023-10-09 15:41:19 +0600

    Vite + Tialwind + React Router + DaisyUI install complete

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtM8BfKqu9dAUfb48HzNGMyEUZAUO2FjU",
+  authDomain: "event-management-auth-d3a82.firebaseapp.com",
+  projectId: "event-management-auth-d3a82",
+  storageBucket: "event-management-auth-d3a82.appspot.com",
+  messagingSenderId: "844526134884",
+  appId: "1:844526134884:web:9b1f6e93ab033bba7ca171"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/layouts/Root.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+const Root = () => {
+    return (
+        <div className='max-w-full mx-auto'>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Root;
